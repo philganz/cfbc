5d2ec1c8622c658b7e09926ef5954ba8312de4cd
Phil Ganz <ganzphil@gmail.com>
2016-07-07 16:18:52 -0800

    plots script cleaned

--- plots.R
+++ plots.R
@@ -1,5 +1,4 @@
 #load required packages
-#library(reshape2)
 library(ggplot2)
 library(gridExtra)
 
@@ -15,30 +14,6 @@ results_sig  <- results
 
 #remove last array
 rm(results)
-
-#remove unneccessary columns
-#results_gam  <- results_gam[,-c(1,3,5,7),,]
-#results_main <- results_main[,-c(1,3,5,7),,]
-#results_sig  <- results_sig[,-c(1,3,5,7),,]
-
-#reshape arrays into long data frames
-#results_gam <- melt(results_gam)
-#results_main <- melt(results_main)
-#results_sig <- melt(results_sig)
-
-#rename columns of data frames
-#colnames(results_gam) <- c("replicate","variable","gamma_true","sigma_eps_true","value")
-#colnames(results_main) <- c("replicate","variable","gamma_true","sigma_eps_true","value")
-#colnames(results_sig) <- c("replicate","variable","gamma_true","sigma_eps_true","value")
-
-#remove unneccessary rows
-#results_gam <- subset(results_gam,results_gam$variable != "gamma_true")
-#results_main <- subset(results_main,results_main$variable != "gamma_true")
-#results_sig <- subset(results_sig,results_sig$variable != "gamma_true")
-#
-#results_gam <- subset(results_gam,results_gam$variable != "sigma_eps_true")
-#results_main <- subset(results_main,results_main$variable != "sigma_eps_true")
-#results_sig <- subset(results_sig,results_sig$variable != "sigma_eps_true")
 
 #stack results from separate cases on top of each other
 results_gam  <- rbind(results_gam[,,"0.1",],results_gam[,,"0.3",],results_gam[,,"0.7",],results_gam[,,"0.9",])
@@ -62,7 +37,10 @@ results_gam$gamma_resids <- results_gam$gamma_est - results_gam$gamma_true
 results_sig$sigma_eps_resids <- results_sig$sigma_eps_est - results_sig$sigma_eps_true
 
 #plots
+
+#save default margins
 d.mar <- c(5.1,4.1,4.1,2.1)
+
 #histograms of parameter estimates
 #initialize plotting function
 pdf("params_hists.pdf",7,5)
@@ -86,40 +64,6 @@ abline(v=mean(results_main$sigma_eps_est),lty=2,lwd=2)
 abline(v=median(results_main$sigma_eps_est),lty=3,lwd=2)
 #turn plotting function off
 dev.off()
-
-#boxplots of gamma
-#ggplot(results_gam,aes(factor(gamma_true),gamma_est))+
-#geom_boxplot()+
-#geom_segment(data=subset(results_gam,results_gam$gamma_true==0.1),aes(x=-Inf,xend=1.375,y=mean(gamma_est),yend=mean(gamma_est)),linetype=2)+
-#geom_segment(data=subset(results_gam,results_gam$gamma_true==0.3),aes(x=-Inf,xend=2.375,y=mean(gamma_est),yend=mean(gamma_est)),linetype=2)+
-#geom_segment(data=subset(results_gam,results_gam$gamma_true==0.5),aes(x=-Inf,xend=3.375,y=mean(gamma_est),yend=mean(gamma_est)),linetype=2)+
-#geom_segment(data=subset(results_gam,results_gam$gamma_true==0.7),aes(x=-Inf,xend=4.375,y=mean(gamma_est),yend=mean(gamma_est)),linetype=2)+
-#geom_segment(data=subset(results_gam,results_gam$gamma_true==0.9),aes(x=-Inf,xend=5.375,y=mean(gamma_est),yend=mean(gamma_est)),linetype=2)+
-#scale_y_continuous(breaks=c(0.1,0.3,0.5,0.7,0.9))+
-#xlab("True")+
-#ylab("Estimated")+
-#labs(title=expression(gamma))+
-#theme_classic(base_size = 8)+
-#coord_flip()
-#save plot
-#ggsave(file="gamma_box.pdf",width=8,height=3)
-
-#boxplots of sigma_eps
-#ggplot(results_sig,aes(factor(sigma_eps_true),sigma_eps_est))+
-#geom_boxplot()+
-#geom_segment(data=subset(results_sig,results_sig$sigma_eps_true==0.1),aes(x=-Inf,xend=1.375,y=mean(sigma_eps_est),yend=mean(sigma_eps_est)),linetype=2)+
-#geom_segment(data=subset(results_sig,results_sig$sigma_eps_true==0.3),aes(x=-Inf,xend=2.375,y=mean(sigma_eps_est),yend=mean(sigma_eps_est)),linetype=2)+
-#geom_segment(data=subset(results_sig,results_sig$sigma_eps_true==0.5),aes(x=-Inf,xend=3.375,y=mean(sigma_eps_est),yend=mean(sigma_eps_est)),linetype=2)+
-#geom_segment(data=subset(results_sig,results_sig$sigma_eps_true==0.7),aes(x=-Inf,xend=4.375,y=mean(sigma_eps_est),yend=mean(sigma_eps_est)),linetype=2)+
-#geom_segment(data=subset(results_sig,results_sig$sigma_eps_true==0.9),aes(x=-Inf,xend=5.375,y=mean(sigma_eps_est),yend=mean(sigma_eps_est)),linetype=2)+
-#scale_y_continuous(breaks=c(0.1,0.3,0.5,0.7,0.9,2,3,4,5,6,7,8))+
-#xlab("True")+
-#ylab("Estimated")+
-#labs(title=expression(sigma[epsilon]))+
-#theme_classic(base_size = 8)+
-#coord_flip()
-#save plot
-#ggsave(file="sigma_eps_box.pdf",width=8,height=3)
 
 #boxplots of gamma and sigma_eps residuals
 #initialize plotting function
